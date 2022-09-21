1a263714ce1def5850a86cf499ca3bf2d865e4cc
py <pyd3.14@gmail.com>
2022-09-21 13:40:28 +0200

    init

---
+++ public/assets/js/app.js
@@ -0,0 +1,14 @@
+var app = {
+
+    init: function () {
+
+        /**
+        * *****************************
+        * L I S T E N E R S
+        * *****************************
+        */
+        $('.sidenav').sidenav();
+    },
+}
+
+document.addEventListener('DOMContentLoaded', app.init)
